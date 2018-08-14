9790a77879a34fedd01ca2cf90209ac8c8d8fa8f
Raghu Betina <raghu@firstdraft.com>
2018-08-14 02:01:10 +0000

    Direct association between classifications and safety_events

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,6 +1,9 @@
 class Classification < ApplicationRecord
   # Direct associations
 
+  has_many   :safety_events,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
